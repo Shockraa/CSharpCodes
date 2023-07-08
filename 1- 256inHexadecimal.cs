72a4336cab3a79916ec6ddcca47c1c777451a119
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-07-08 13:50:21 -0300

    256 -> Hexadecimal
      Código que converte o número 256 em hexadecimal (Fundamentals of Computer Programming with C# Exercise)

---
+++ 1- 256inHexadecimal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hexadecimal
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            /* Initialize a variable of type int with a value of 256 in
+            hexadecimal format (256 is 100 in a numeral system with base 16). */
+            int valor3 = 256 % 16;
+            int valor2 = 16 % 16;
+            int valor1 = 1 % 16;
+            
+            valor1.ToString();
+            valor2.ToString();
+            valor3.ToString();
+
+            Console.Write(valor1);
+            Console.Write(valor2);
+            Console.Write(valor3);
+            Console.ReadKey();
+        }
+    }
+}
