85ddaf08b49951d0ffb16c8068f7301fb7b9bf3c
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-07-19 19:20:38 -0300

    Programa que converte número decimal em binário e vice-versa.

--- 12- BinaryConverter.cs
+++ 12- BinaryConverter.cs
@@ -5,9 +5,11 @@ class ConversorBinário
 	static void Main()
 	{
 		//Write a program that converts a decimal number to binary one.
+		//Write a program that converts a binary number to decimal one.
 		
-		Console.WriteLine("Olá amigo, como você deseja converter (digite o número correspondente");
+		Console.WriteLine("Olá amigo, como você deseja converter (digite o número correspondente)");
 		Console.WriteLine("1- Decimal para binário");
+		Console.WriteLine("2- Binário para decimal");
 		int escolha = int.Parse(Console.ReadLine());
 		if (escolha == 1)
 		{
@@ -15,6 +17,17 @@ class ConversorBinário
 			int numeroDecimal = int.Parse(Console.ReadLine());
 			string numeroBinario = Convert.ToString(numeroDecimal, 2);
 			Console.WriteLine("Número binário: " + numeroBinario);
+		}
+		else if (escolha == 2)
+		{
+			Console.WriteLine("Insira o número binário");
+			string numeroBinario = Console.ReadLine();
+             int numeroDecimal = Convert.ToInt32(numeroBinario, 2);
+             Console.WriteLine(numeroDecimal); 
+		}
+		else
+		{
+			Console.WriteLine("Da para escolher uma das opções?, por sua causa você terá que executar o programa outra vez kkkk");
 		}
 	}
 }
