14a682a1d178444f5f95ca8ca6c361b8701fc837
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-07-24 16:50:18 -0300

    Update longestPath.cs

--- longestPath.cs
+++ longestPath.cs
@@ -1,32 +1,34 @@
 using System;
 
-public class Program
+public class LabirintoSaidaLonga
 {
-    static char[,] lab = {
-        {' ', ' ', ' ', ' ', ' ', '*', ' '},
+    //You are given a matrix with passable and impassable cells. Write a program that finds the largest area of neighboring passable cells.
+    //O Labirinto checa todos os caminhos possíveis e armazena o maior deles.
+    static char[,] lab = { 
+        {' ', ' ', ' ', ' ', ' ', '*', ' '}, //O labirinto pode ser modificado contanto que haja uma saída
         {'*', '*', '*', '*', ' ', '*', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', ' '},
         {'*', '*', '*', ' ', '*', '*', ' '},
         {' ', ' ', ' ', ' ', ' ', ' ', 'e'},
     };
     
-	static string longestPath = "";
+	static string longestPath = ""; //String que armazena as direções do caminho mais longo
     public static void Main()
     {
-        Caminho(0, 0, " ");
-		Console.WriteLine("Caminho mais transitável: " + longestPath);
+        Caminho(0, 0, " "); //Ativa método
+		Console.WriteLine("Caminho mais transitável: " + longestPath); //Resposta
     }
 
     static void Caminho(int lin, int col, string path)
     {
-        if (lin < 0 || lin >= lab.GetLength(0) || col < 0 || col >= lab.GetLength(1))
+        if (lin < 0 || lin >= lab.GetLength(0) || col < 0 || col >= lab.GetLength(1)) //Condição que impede a saida do labirinto
         {
             return;
         }
 
-        if (lab[lin, col] == 'e')
+        if (lab[lin, col] == 'e')  //Condição que verifica se chegou na saída
         {
-            if (path.Length > longestPath.Length)
+            if (path.Length > longestPath.Length) //Verifica se o caminho atual é o maior
             {
                 longestPath = path; // Atualiza o caminho mais longo encontrado
             }
@@ -41,13 +43,13 @@ public class Program
 
         lab[lin, col] = 's'; // Marca a célula como visitada
 
-        // Chamada recursiva para todas as direções checando todas as possibilidades e guardando as direções feitasqqqq
+        // Chamada recursiva para todas as direções checando todas as possibilidades e guardando as direções feitas
         Caminho(lin, col - 1, path + " esquerda "); 
         Caminho(lin - 1, col, path + " cima "); 
         Caminho(lin, col + 1, path + " direita "); 
         Caminho(lin + 1, col, path + " baixo "); 
 
-        // Marca de volta a célula como não visitada (backtracking)
+        // Marca de volta a célula como não visitada para que seja possível o backtracking
         lab[lin, col] = ' ';
     }
 }
