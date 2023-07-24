c75ccbc9ce022759e3823eccb17ec4aaabdac1c8
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-07-24 15:25:35 -0300

    Programa recursivo que faz permutação com loops.

--- 18- RecursivePermulation.cs
+++ 18- RecursivePermulation.cs
@@ -4,21 +4,21 @@ public class Permulation
 {
         //Write a recursive program, which generates and prints all permutations of the numbers 1, 2, …, n, for a given integer n.
 	
-	static int[] resposta;
+	static int[] resposta; 
 	static int n;
 	static bool[] numerosUsados; // Array para acompanhar os números que já foram usados
 	public static void Main()
 	{
 		Console.WriteLine("N?");
 		n = int.Parse(Console.ReadLine());
-		resposta = new int[n];
-		numerosUsados = new bool[n + 1]; // Inicializa o array com todos os valores como false
-		GetResposta(0);
+		resposta = new int[n]; //Define a largura como n
+		numerosUsados = new bool[n + 1]; // Inicializa o array com todos os valores false
+		GetResposta(0); //Ativa método
 	}
 
-	public static void GetResposta(int loopAtual)
+	public static void GetResposta(int loopAtual) //Declara
 	{
-		if (loopAtual == n)
+		if (loopAtual == n) //Condição que mostra na tela e faz a recursão atual encerrar
 		{
 			MostrarLoop();
 			return;
@@ -26,11 +26,11 @@ public class Permulation
 
 		for (int i = 1; i <= n; i++)
 		{
-			if (!numerosUsados[i])
+			if (!numerosUsados[i]) //Caso o número não seja usado ele é atribuido a resposta
 			{
 				resposta[loopAtual] = i;
 				numerosUsados[i] = true; // Marca o número como usado
-				GetResposta(loopAtual + 1);
+				GetResposta(loopAtual + 1); //Ativa recursão
 				numerosUsados[i] = false; // Desmarca o número para que possa ser usado em outra combinação
 			}
 		}
@@ -40,7 +40,7 @@ public class Permulation
 	{
 		for (int i = 0; i < n; i++)
 		{
-			Console.Write("{0} ", resposta[i]);
+			Console.Write("{0} ", resposta[i]); 
 		}
 
 		Console.WriteLine();
