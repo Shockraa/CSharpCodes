2a37e9081b60d9a5644ae25881aaa2e8a843d8aa
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-08-08 20:25:36 -0300

    Update and rename 22- ProgramaBancario.cs to 22- NamespaceBancario.cs

---
+++ 22- NamespaceBancario.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBancario
+{
+   
+    public class Conta
+    {
+        public double saldo;
+        public int numero;
+
+    public Conta()  //primeiro parametro
+    {
+        this.saldo = 0;
+        this.numero = 0;
+    }
+     public Conta(double saldoInicial) //segundo parametro
+    {
+        if (saldoInicial >= 0)
+        {
+            saldo = saldoInicial;
+        }
+        else
+        {
+            saldo = 0;
+        }
+        numero = 0;
+    }
+    public Conta(int numeroConta) { //terceiro parametro
+        saldo = 0;
+        numero = numeroConta;
+    }
+    public void depositar(double valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+            Console.WriteLine("Depósito realizado com sucesso"); //True
+        } 
+        else {
+           Console.WriteLine("Valor inválido para depósito"); //False
+        }
+    }
+    public string consultarSaldo() {
+        return "Saldo atual: R$" + this.saldo;
+    }
+    public void Depositar(double valor)
+    {
+        if (valor > 0)
+        {
+            saldo += valor;
+            Console.WriteLine("Depósito realizado");
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido");
+        }
+    }
+
+    public bool Sacar(double valor)
+    {
+        if (valor > 0 && saldo >= valor)
+        {
+            saldo -= valor;
+            Console.WriteLine("Saque realizado");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Saldo insuficiente ou saque inválido");
+            return false;
+        }
+    }
+
+    public string ConsultarSaldo()
+    {
+        return "Saldo atual: R$" + saldo;
+    }
+
+
+    }
+}
