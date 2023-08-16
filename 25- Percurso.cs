e34819e100354d534d9e6b4bbd8885d4bd5fad1f
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-08-16 15:16:40 -0300

    Programa que calcula custos de um percurso usando classes

--- 25- Percurso.cs
+++ 25- Percurso.cs
@@ -4,8 +4,8 @@ public class Program
 {
 	public static void Main()
 	{
-		Percurso percurso = new Percurso();
-		Custos custos = new Custos();
+		Percurso percurso = new Percurso(); //Cria objeto
+		Custos custos = new Custos(); //Cria outro objeto
 		int escolha = 4; //Declara um valor não ultilizado
 		while (escolha != 0)
 		{
@@ -18,18 +18,18 @@ public class Program
 			if (escolha == 1)
 			{
 				Console.WriteLine("Insira km percorrido");
-				percurso.kmPercorrida = double.Parse(Console.ReadLine());
+				percurso.kmPercorrida = double.Parse(Console.ReadLine()); 
 				Console.WriteLine("Insira valor do combustível");
 				percurso.valorCombustivel = double.Parse(Console.ReadLine());
 				Console.WriteLine("Insira valor do pedagio");
 				percurso.valorPedagio = double.Parse(Console.ReadLine());
-				percurso = new Percurso(percurso.kmPercorrida, percurso.valorCombustivel, percurso.valorPedagio);
+				percurso = new Percurso(percurso.kmPercorrida, percurso.valorCombustivel, percurso.valorPedagio); //Ativa o construtor com 3 parametros
 			}
 
 			if (escolha == 2)
 			{
 				Console.WriteLine("");
-				Console.WriteLine("Km percorrido: " + Percurso.listarPercurso(percurso.kmPercorrida) + "km");
+				Console.WriteLine("Km percorrido: " + Percurso.listarPercurso(percurso.kmPercorrida) + "km"); //Mostra na tela
 				Console.WriteLine("Valor combustível: " + Percurso.listarPercurso(percurso.valorCombustivel) + "L");
 				Console.WriteLine("Valor pedágio: " + Percurso.listarPercurso(percurso.valorPedagio) + "R$");
 				Console.WriteLine("");
@@ -38,11 +38,11 @@ public class Program
 			if (escolha == 3)
 			{
 				Console.WriteLine("");
-				Console.WriteLine("Custo total: " + Percurso.totalPercurso + "R$");
+				Console.WriteLine("Custo total: " + Percurso.totalPercurso + "R$"); //Mostra na tela
 			}
 		}
 
-		Console.WriteLine("Obrigado por usar!");
+		Console.WriteLine("Obrigado por usar!"); //Fim do programa
 	}
 }
 
@@ -51,8 +51,8 @@ public class Percurso
 	public double kmPercorrida;
 	public double valorCombustivel;
 	public double valorPedagio;
-	public static string totalPercurso;
-	public Percurso()
+	public static string totalPercurso; //Estatico para facilitar
+	public Percurso() //Primeiro construtor
 	{
 		this.kmPercorrida = 0;
 		this.valorCombustivel = 0;
@@ -60,17 +60,17 @@ public class Percurso
 		totalPercurso = "0";
 	}
 
-	public Percurso(double kmPercorrida, double valorCombustivel, double valorPedagio)
+	public Percurso(double kmPercorrida, double valorCombustivel, double valorPedagio) //Segundo construtor
 	{
 		this.kmPercorrida = kmPercorrida;
 		this.valorCombustivel = valorCombustivel;
 		this.valorPedagio = valorPedagio;
-		totalPercurso = Custos.calcularViagem(kmPercorrida, valorCombustivel, valorPedagio, totalPercurso);
+		totalPercurso = Custos.calcularViagem(kmPercorrida, valorCombustivel, valorPedagio, totalPercurso); //Ativa método da classe custos
 	}
 
 	public static string listarPercurso(double variavel)
 	{
-		return variavel.ToString();
+		return variavel.ToString(); //Converte para string
 	}
 }
 
