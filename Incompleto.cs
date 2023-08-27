31af32874f373515a87e7081e40f71945288bbc8
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-08-27 15:52:17 -0300

    Update Incompleto.cs

--- Incompleto.cs
+++ Incompleto.cs
@@ -2,112 +2,130 @@ using System;
 
 public class Program
 {
-	public static void Main()
-	{
-		Comanda controleMesa = new Comanda(); //Cria objeto
-		Caixa caixa = new Caixa(); //Cria outro objeto
-		int escolha = 5; //Declara um valor não ultilizado
-		while (escolha != 0)
-		{
-			Console.WriteLine("Olá, escolha uma das opções:");
-			Console.WriteLine("1 – Lançar gastos");
-			Console.WriteLine("2 – Verificar promoção");
-			Console.WriteLine("3 – Fechar mesa");
-			Console.WriteLine("4 – Listar comanda");
-			Console.WriteLine("0- Sair");
-			escolha = int.Parse(Console.ReadLine());
-			if (escolha == 1)
-			{
-				Comanda.lancarGastos();
-			}
+    public static void Main()
+    {
+        Comanda controleMesa = new Comanda();
+        Caixa caixa = new Caixa();
+        int escolha = 5;
 
-			if (escolha == 2)
-			{
-				Comanda.verificarPromocao();
-			}
+        while (escolha != 0)
+        {
+            Console.WriteLine("Olá, escolha uma das opções:");
+            Console.WriteLine("1 – Lançar gastos");
+            Console.WriteLine("2 – Verificar promoção");
+            Console.WriteLine("3 – Fechar mesa");
+            Console.WriteLine("4 – Listar comanda");
+            Console.WriteLine("0 - Sair");
+            escolha = int.Parse(Console.ReadLine());
 
-			if (escolha == 3)
-			{
-				Caixa.totalizarComanda(controleMesa);
-			}
+            if (escolha == 1)
+            {
+                controleMesa.LancarGastos();
+            }
 
-			if (escolha == 4)
-			{
-			//Opção mostrar (ignorar)
-			}
-		}
+            if (escolha == 2)
+            {
+                controleMesa.VerificarPromocao();
+            }
 
-		Console.WriteLine("Obrigado por usar!"); //Fim do programa
-	}
+            if (escolha == 3)
+            {
+                Console.WriteLine("Valor total da comanda: " + Caixa.TotalizarComanda(controleMesa));
+            }
+
+            if (escolha == 4)
+            {
+                controleMesa.ListarComanda();
+            }
+        }
+
+        Console.WriteLine("Obrigado, volte sempre!");
+    }
 }
 
 public class Comanda
 {
-	private int id;
-	private static double valorConsumo;
-	private double valorEstacionamento;
-	private double valorCouvert;
-	private static double valorDescontoPromocional;
-	private double valorTotal;
-	
-	public Comanda()
-	{
-		this.id = 0; //Exemplo
-		valorConsumo = 0;
-		this.valorEstacionamento = 0;
-		this.valorCouvert = 0;
-	}
+    private int id;
+    private double valorConsumo;
+    private double valorEstacionamento;
+    private double valorCouvert;
+    private double valorDescontoPromocional;
+    private double valorTotal;
 
-	public Comanda(double refvalorConsumo, double valorEstacionamento, double valorCouvert)
-	{
-		valorDescontoPromocional = 0;
-		this.valorTotal = 0;
-		valorConsumo = refvalorConsumo;
-		this.valorEstacionamento = valorEstacionamento;
-		this.valorCouvert = valorCouvert;
-	}
-	public Comanda(int id)
-	{
-		this.id = 0;
-	}
+    public Comanda()
+    {
+        this.id = 0;
+        this.valorConsumo = 0;
+        this.valorEstacionamento = 0;
+        this.valorCouvert = 0;
+    }
 
-	public static void lancarGastos()
-	{
-		Console.WriteLine("Valor consumido?");
-		double refvalorConsumo = double.Parse(Console.ReadLine());
-		Console.WriteLine("Valor estacionamento?");
-		double valorEstacionamento = double.Parse(Console.ReadLine());
-		Console.WriteLine("Valor couvert?");
-		double valorCouvert = double.Parse(Console.ReadLine());
-		Comanda controleMesa = new Comanda(refvalorConsumo, valorEstacionamento, valorCouvert); //Segundo construtor
-		Console.WriteLine("Os gastos foram lançados.");
-	}
+    public Comanda(double refvalorConsumo, double valorEstacionamento, double valorCouvert)
+    {
+        this.valorDescontoPromocional = 0;
+        this.valorTotal = 0;
+        this.valorConsumo = refvalorConsumo;
+        this.valorEstacionamento = valorEstacionamento;
+        this.valorCouvert = valorCouvert;
+    }
 
-	public static void verificarPromocao()
-	{
-		if (valorConsumo > 100)
-		{
-			Console.WriteLine("Você recebeu um desconto de 10%");
-			valorDescontoPromocional = valorConsumo / 10;
-		}
-		else
-		{
-			Console.WriteLine("Sem desconto >:( ");
-		}
-		
-	}
+    public Comanda(int id)
+    {
+        this.id = 0;
+    }
 
-	public static void listarComanda()
-	{
-	}
+    public void LancarGastos()
+    {
+        Console.WriteLine("Valor consumido?");
+        double refvalorConsumo = double.Parse(Console.ReadLine());
+        Console.WriteLine("Valor estacionamento?");
+        double valorEstacionamento = double.Parse(Console.ReadLine());
+        Console.WriteLine("Valor couvert?");
+        double valorCouvert = double.Parse(Console.ReadLine());
+
+        // Atribuir os valores aos atributos da comanda atual
+        this.valorConsumo = refvalorConsumo;
+        this.valorEstacionamento = valorEstacionamento;
+        this.valorCouvert = valorCouvert;
+
+        Console.WriteLine("Os gastos foram lançados.");
+    }
+
+    public void VerificarPromocao()
+    {
+        if (this.valorConsumo > 100)
+        {
+            Console.WriteLine("Você recebeu um desconto de 10%");
+            this.valorDescontoPromocional = this.valorConsumo * 0.1;
+			this.valorTotal = this.valorTotal - this.valorDescontoPromocional;
+        }
+        else
+        {
+            Console.WriteLine("Sem desconto >:(");
+        }
+    }
+
+    public void ListarComanda()
+    {
+        Console.WriteLine($"Valor Consumo: {this.valorConsumo}");
+        Console.WriteLine($"Valor Estacionamento: {this.valorEstacionamento}");
+        Console.WriteLine($"Valor Couvert: {this.valorCouvert}");
+        Console.WriteLine($"Desconto Promocional: {this.valorDescontoPromocional}");
+        this.valorTotal = this.valorConsumo + this.valorEstacionamento + this.valorCouvert - this.valorDescontoPromocional;
+        Console.WriteLine($"Valor Total: {this.valorTotal}");
+    }
+
+    public double ValorTotal
+    {
+        get { return this.valorTotal; }
+    }
 }
 
 public class Caixa
 {
-	public static void totalizarComanda(Comanda controleMesa)
+    public static double TotalizarComanda(Comanda controleMesa)
     {
-        double total = Comanda.ValorTotal;
-
-        Console.WriteLine("Total da comanda: " + total);
+        double total = controleMesa.ValorTotal;
+        return total;
     }
 }
