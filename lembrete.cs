dbbc0380b85ed6b0d905255fa1bbf9c883fd2bb6
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-09-11 19:34:46 -0300

    Update lembrete.cs

--- lembrete.cs
+++ lembrete.cs
@@ -7,3 +7,15 @@ public class Program
 		Console.WriteLine("Hello World");
 	}
 }
+
+/* ignorar
+
+Magic Cylinder
+Magic Janner
+Metalmorph
+Mirror Force
+Negate Attack
+Seven tools of the bandit
+Torrential tribute
+Trap hole
+Widespread ruin
