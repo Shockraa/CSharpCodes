3a8f03950d87e10ea74d2c60d690d817f0bad924
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-09-25 19:08:40 -0300

    Desistência

--- FuturoYuGiOh.cs
+++ FuturoYuGiOh.cs
@@ -1,22 +1,3 @@
-Gusing System;
-					
-public class Program
-{
-	public static void Main()
-	{
-		Console.WriteLine("Hello World");
-	}
-}
-
-/* ignorar
-
-Magic Cylinder
-Magic Janner
-Mirror Force
-Negate Attack
-Seven tools of the bandit
-Torrential tribute
-Widespread ruin
 
 
 
