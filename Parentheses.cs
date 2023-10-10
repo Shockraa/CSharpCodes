8151bd2bf2737d6de2df6600afafe8a884303a97
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-10-10 20:30:08 -0300

    Update Parentheses.cs

--- Parentheses.cs
+++ Parentheses.cs
@@ -1,1 +1,32 @@
-
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{ 
+		Console.WriteLine("Insira uma expressão numérica");
+		string exp = Console.ReadLine();
+        char[] charArray = exp.ToCharArray();
+		int contador = 0;
+		
+		for (int i = 0; i < charArray.Length; i++)
+		{
+			if (charArray[i] == '(')
+			{
+				contador++;
+			}
+			if (charArray[i] == ')')
+			{
+				contador--;
+			}
+		}
+		if (contador == 0)
+		{
+		   Console.WriteLine("É uma expressão numérica valida");
+		}
+		else
+		{
+			Console.WriteLine("Não é uma expressão numérica valida");
+		}
+	}
+}
