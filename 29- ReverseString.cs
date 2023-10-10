a760f84de3982f007b66278fcaf477efc7f1c3ee
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-10-09 22:08:40 -0300

    Update 29- ReverseString.cs

--- 29- ReverseString.cs
+++ 29- ReverseString.cs
@@ -1,19 +1,19 @@
 using System;
-using System.Text;
+using System.Text; //Importação para usar StringBuilder
 
 public class Program
 {
-//2
+//Ex 2
     public static void Main()
     {
         Console.WriteLine("Insira qualquer coisa para inverter");
         string resposta = Console.ReadLine();
-        Console.WriteLine(Reverso(resposta));
+        Console.WriteLine(Reverso(resposta)); //Método do StringBuilder
     }
 
     static string Reverso(string resposta)
     {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(); 
         for (int i = resposta.Length - 1; i >= 0; i--)
         {
             sb.Append(resposta[i]);
