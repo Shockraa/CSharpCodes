96246d95a8ef7f4e4b7a5496aae6e37ce1573362
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-10-11 19:26:04 -0300

    Update file.cs

--- file.cs
+++ file.cs
@@ -1,1 +1,21 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+    //Lembrete ex 5
+    
+		Console.WriteLine("Insira uma frase");
+		string quote = Console.ReadLine();
+		quote.ToLower();
+		string palavraChave = "in";
+		int num = quote.IndexOf(palavraChave);
+		while (num != -1)
+		{
+			Console.WriteLine("{0} foi encontrado no num: {1}", palavraChave, num);
+			num = quote.IndexOf(palavraChave, num + 1);
+		}
+	}
+}
 
