075efc92c8b19666d97bfb1c52a76e88ea467f4e
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-10-11 19:28:53 -0300

    Update UPCASEHTML.cs

--- UPCASEHTML.cs
+++ UPCASEHTML.cs
@@ -1,1 +1,26 @@
+using System;
+using System.Text.RegularExpressions;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Digite uma frase");
+		string frase = Console.ReadLine();
+		string modificada = Modificar(frase);
+		 Console.WriteLine(modificada);
+	}
+	public static string Modificar(string frase)
+	{
+		string seq = @"<upcase>(.*?)</upcase>";
+		string result = Regex.Replace(frase, seq, match =>
+        {
+            string content = match.Groups[1].Value;
+            return content.ToUpper();
+        });
+
+        return result;
+		
+	}
+}
 
