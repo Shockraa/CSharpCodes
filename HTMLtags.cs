7d46c2ef0e272f88f28cded3f917680f752e7932
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-10-13 17:47:47 -0300

    Update HTMLtags.cs

--- HTMLtags.cs
+++ HTMLtags.cs
@@ -1,1 +1,13 @@
+using System;
+using System.Text.RegularExpressions;		
+public class Program
+{
+	public static void Main()
+	{
+		string html = "<body><h1>Eu sou eu</h1></body><script>lololol</script>";
+		string pattern = "<.*?>";
+		string textoLimpo = Regex.Replace(html, pattern, string.Empty);
+		Console.WriteLine(textoLimpo);
+	}
+}
 
