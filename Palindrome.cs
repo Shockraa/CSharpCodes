04db2427ed8ceeb778c8166a271a7287f712d1f9
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-10-14 14:26:42 -0300

    Update Palindrome.cs

--- Palindrome.cs
+++ Palindrome.cs
@@ -1,1 +1,31 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Insira uma frase para encontrar palavras palíndromes:");
+        string frase = Console.ReadLine();
+
+        string[] palavras = frase.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string palavra in palavras)
+        {
+            if (Verificar(palavra))
+            {
+                Console.WriteLine(palavra);
+            }
+        }
+    }
+
+    public static bool Verificar(string palavra)
+    {
+        palavra = palavra.Replace(" ", "").ToLower(); //Necessário
+        char[] arr = palavra.ToCharArray();
+        Array.Reverse(arr);
+        string palavraReversa = new string(arr);
+
+        return palavra == palavraReversa;
+    }
+}
 
