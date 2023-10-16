68d3f63b1740067f32915b8464ccbee98864bb24
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-10-15 21:59:36 -0300

    Update Censura.cs

--- Censura.cs
+++ Censura.cs
@@ -1,1 +1,33 @@
+using System;
+using System.Text.RegularExpressions;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Insira uma frase");
+		string frase = Console.ReadLine();
+		Console.WriteLine("Insira as palavras para censurar (separe-as com virgula de preferência)");
+        string censura = Console.ReadLine();
+		
+		string resultado = Censurar(frase, censura);
+        
+        Console.WriteLine(resultado);
+    }
+
+    static string Censurar(string text, string censura)
+    {
+
+        string[] wordsArray = censura.Split(',');
+
+
+        string seq = string.Join("|", wordsArray) ;  //operador lógico para diferenciar seq
+
+       
+        string result = Regex.Replace(text, seq, match => new string('*', match.Length));
+
+        return result;
+    }
+	
+}
 
