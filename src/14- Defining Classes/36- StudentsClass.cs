f78b99451e5226eebf7ed01dd16852b4b7767beb
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-10-16 20:04:40 -0300

    Programa que gera estudantes usando classes e Enum.

--- src/14- Defining Classes/36- StudentsClass.cs
+++ src/14- Defining Classes/36- StudentsClass.cs
@@ -1,13 +1,12 @@
 using System;
 using System.Collections.Generic;
 
+//Ex 1 ao 7 no Cap 14 do livro, simula estudantes
 public class Program
 {
 	public static void Main()
 	{
-		
-		
-		
+		StudentTest.CreateStudents();
 	}
 }
 
@@ -15,15 +14,10 @@ public class Student
 {
 	//Fields
 	private string fullName;
-
 	private string course;
-
 	private string email;
-
 	private int phoneNumber;
-
 	private Subject subject;
-
 	private University university;
 	
 //getters e setters
@@ -33,9 +27,8 @@ public class Student
 	public int PhoneNumber { get => phoneNumber; set => phoneNumber = value;}
 	public Subject Subject { get => subject; set => subject = value;}
 	public University University { get => university; set => university = value;}
+	public static int studentsNumber = 0; //Contador de estudantes
 	
-
-	private static int studentsNumber = 0; //Contador de estudantes
 	public Student() //Primeiro construtor
 	{
 		this.fullName = null;
@@ -53,16 +46,18 @@ public class Student
 		this.university = university;
 		this.email = email;
 		this.phoneNumber = phoneNumber;
+		studentsNumber++;
 	}
 
 	public void StudentInfo() //Método para mostrar informação do estudante matriculado
 	{
-		Console.WriteLine("Name:" + this.fullName);
-		Console.WriteLine("Course:" + this.course);
-		Console.WriteLine("Subject:" + this.subject);
-		Console.WriteLine("University:" + this.university);
-		Console.WriteLine("Email:" + this.email);
-		Console.WriteLine("Phone number:" + this.phoneNumber);
+		Console.WriteLine("Name: " + this.fullName);
+		Console.WriteLine("Course: " + this.course);
+		Console.WriteLine("Subject: " + this.subject);
+		Console.WriteLine("University: " + this.university);
+		Console.WriteLine("Email: " + this.email);
+		Console.WriteLine("Phone number: " + this.phoneNumber);
+		Console.WriteLine(""); //Pula linha
 	}
 		
 } 
@@ -70,19 +65,28 @@ public class Student
 public class StudentTest
 {
 	
-	private static List<Student> students = new List<Student>();
+	private static List<Student> students = new List<Student>(); //Declara lista para armazenar estudantes dentro
 
     public static List<Student> Students { get => students; }
 
     public static void CreateStudents()
     {
         Student student1 = new Student("Gustavo", "DS", Subject.Portuguese, University.USP, "juicewrld@gmail.com", 1166699966);
-        Student student2 = new Student("Maria", "Engenharia Civil", Subject.Spanish, University.Harvard, "maria@gmail.com", 1155544433);
-        Student student3 = new Student("João", "Medicina", Subject.Math, University.Stanford, "joao@gmail.com", 1122211100);
-
-        students.Add(student1);
-        students.Add(student2);
-        students.Add(student3);
+        Student student2 = new Student("Hevandrinho", "Trabalhador justo e honesto", Subject.Spanish, University.Harvard, "rego@gmail.com", 1155544433);
+        Student student3 = new Student("João", "Web Dev", Subject.Math, University.Stanford, "joao@gmail.com", 1122211100);
+		students.Add(student1);
+		students.Add(student2);
+		students.Add(student3);
+		StartStudentInfo();
+		Console.WriteLine("Total number of students :" + Student.studentsNumber);
+		
+    }
+	public static void StartStudentInfo()
+    {
+        foreach (Student student in students)
+        {
+            student.StudentInfo();
+        }
     }
 		
 		
