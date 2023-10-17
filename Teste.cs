b5a40914c5a0254427e55b444809ba99ccc976f8
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-10-16 22:15:51 -0300

    Create Teste.cs

---
+++ Teste.cs
@@ -0,0 +1,32 @@
+namespace Mobile
+{
+
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+
+    public class GSM
+    {
+        private string phoneModel;
+        private string manufacturer;
+        private double price;
+        private string owner;
+        
+    }
+    public class Battery
+    {
+        private string batteryModel;
+        private double idleTime;
+    }
+    public class Display
+    {
+
+    }
+}
