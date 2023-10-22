aefd97ec50ccdfdf1061af39f09287599e35389e
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-10-21 22:49:49 -0300

    Update and rename Teste.cs to src/14- Defining Classes/Teste.cs

---
+++ src/14- Defining Classes/Teste.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+	}
+}
+
+public class GSM
+{
+	private string phoneModel;
+	private string manufacturer;
+	private double price;
+	private string owner;
+}
+
+public class Battery
+{
+	private string batteryModel;
+	private double idleTime;
+}
+
+public class Display
+{
+	private double size;
+	private string color;
+}
