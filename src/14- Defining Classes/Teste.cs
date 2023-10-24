07fd429e8d6d106a08f869cdef28719a98e60bda
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-10-24 20:40:12 -0300

    Update Teste.cs

--- src/14- Defining Classes/Teste.cs
+++ src/14- Defining Classes/Teste.cs
@@ -4,7 +4,7 @@ public class Program
 {
 	public static void Main()
 	{
-		Console.WriteLine("Hello World");
+		GSM.DisplayNokiaN95Info();
 	}
 }
 
@@ -14,12 +14,14 @@ public class GSM
 	private string manufacturer;
 	private double price;
 	private string owner;
-
 	public string PhoneModel { get => phoneModel; set => phoneModel = value; }
+
 	public string Manufacturer { get => manufacturer; set => manufacturer = value; }
+
 	public double Price { get => price; set => price = value; }
+
 	public string Owner { get => owner; set => owner = value; }
-	
+
 	public GSM()
 	{
 		this.phoneModel = null;
@@ -27,6 +29,7 @@ public class GSM
 		this.price = 0;
 		this.owner = null;
 	}
+
 	public GSM(string phoneModel, string manufacturer, double price, string owner)
 	{
 		this.phoneModel = phoneModel;
@@ -34,46 +37,61 @@ public class GSM
 		this.price = price;
 		this.owner = owner;
 	}
+
+	private static GSM nokiaN95 = new GSM("Nokia N95", "Nokia", 500, null);
+	public static void DisplayNokiaN95Info()
+	{
+		Console.WriteLine($"Phone model: {nokiaN95.PhoneModel}");
+		Console.WriteLine($"Manufacturer: {nokiaN95.Manufacturer}");
+		Console.WriteLine($"Price: {nokiaN95.Price}");
+	}
 }
 
 public class Battery
 {
-	private string batteryModel;
-	private double idleTime;
+    private BatteryType batteryModel;
+    private double idleTime;
 
-	public string BatteryModel { get => batteryModel; set => batteryModel = value; }
-	public double IdleTime { get => idleTime; set => idleTime = value; }
-	
-	public Battery()
-	{
-		this.batteryModel = null;
-		this.idleTime = 0;
-	}
-	public Battery(string batteryModel, string idleTime)
-	{
-		this.batteryModel = batteryModel;
-		this.idleTime = idleTime;
-	}
+    public BatteryType BatteryModel { get => batteryModel; set => batteryModel = value; }
+    public double IdleTime { get => idleTime; set => idleTime = value; }
+
+    public Battery()
+    {
+        this.batteryModel = BatteryType.LiIon;
+        this.idleTime = 0;
+    }
+
+    public Battery(BatteryType batteryModel, double idleTime)
+    {
+        this.batteryModel = batteryModel;
+        this.idleTime = idleTime;
+    }
 }
 
 public class Display
 {
 	private double size;
 	private string color;
-
 	public double Size { get => size; set => size = value; }
+
 	public string Color { get => color; set => color = value; }
-	
-	
-	
+
 	public Display()
 	{
 		this.size = 0;
-	    this.color = null;
+		this.color = null;
 	}
+
 	public Display(double size, string color)
 	{
 		this.size = size;
-	    this.color = color;
+		this.color = color;
 	}
+}
+
+enum BatteryType
+{
+	LiIon,
+	NiMH,
+	NiCd
 }
