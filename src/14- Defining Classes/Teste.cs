f085e37097ff87c56ce8443940c42d287055746d
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-10-24 16:48:26 -0300

    Update Teste.cs

--- src/14- Defining Classes/Teste.cs
+++ src/14- Defining Classes/Teste.cs
@@ -14,16 +14,27 @@ public class GSM
 	private string manufacturer;
 	private double price;
 	private string owner;
+
+	public string PhoneModel { get => phoneModel; set => phoneModel = value; }
+	public string Manufacturer { get => manufacturer; set => manufacturer = value; }
+	public double Price { get => price; set => price = value; }
+	public string Owner { get => owner; set => owner = value; }
 }
 
 public class Battery
 {
 	private string batteryModel;
 	private double idleTime;
+
+	public string BatteryModel { get => batteryModel; set => batteryModel = value; }
+	public double IdleTime { get => idleTime; set => idleTime = value; }
 }
 
 public class Display
 {
 	private double size;
 	private string color;
+
+	public double Size { get => size; set => size = value; }
+	public string Color { get => color; set => color = value; }
 }
