ed65b0b6d30f734355fb4b778cc68b5198a5da4b
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-10-24 21:54:57 -0300

    Create Divisor.cs

---
+++ src/14- Defining Classes/Divisor.cs
@@ -0,0 +1,14 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+	}
+}
+public class Fraction
+{
+	private decimal numerator;
+	private decimal denominator;
+}
