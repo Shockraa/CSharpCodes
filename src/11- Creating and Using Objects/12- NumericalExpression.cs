4e719b154fc19611a0a43e0447d09c2579d7cf0a
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-11-17 19:29:18 -0300

    Programa que calcula uma expressão numérica.

--- src/11- Creating and Using Objects/12- NumericalExpression.cs
+++ src/11- Creating and Using Objects/12- NumericalExpression.cs
@@ -5,18 +5,14 @@ public class Program
 {
     public static void Main()
     {
+        //Write a program, which calculates the value of a given numeral expression given as a string.
+
         Console.WriteLine("Insira uma expressão numérica");
         string conta = Console.ReadLine();
         DataTable dt = new DataTable();
 
-        try
-        {
             object resultado = dt.Compute(conta, "");
             Console.WriteLine("Resultado: " + resultado);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Erro: " + ex.Message);
-        }
+        
     }
 }
