793181674c0658eaff2d8a19347782534a45ea68
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-11-17 19:43:06 -0300

    Program that hides text with "*"

--- src/13- Strings and Text Processing/07- Censoring.cs
+++ src/13- Strings and Text Processing/07- Censoring.cs
@@ -4,6 +4,9 @@ public class Program
 {
 	public static void Main()
 	{
+                /*Write a program that reads a string from the console (20 characters
+                  maximum) and if shorter complements it right with "*" to 20 characters.*/
+		
 		Console.WriteLine("Insira uma string");
 		string str = Console.ReadLine();
 		char pad = '*';
