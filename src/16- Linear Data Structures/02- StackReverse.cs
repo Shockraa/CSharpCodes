468ba44a05460e01faa64d44fc07096f8f13c5fc
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-11-19 13:42:09 -0300

    Program that prints given numbers in reverse order using stack.

--- src/16- Linear Data Structures/02- StackReverse.cs
+++ src/16- Linear Data Structures/02- StackReverse.cs
@@ -5,28 +5,28 @@ public class Program
 {
     public static void Main()
     {
-        int counter = 0;
-        Stack<int> stack = new Stack<int>();
+        /* Write a program, which reads from the console N integers and prints
+         them in reversed order. Use the Stack<int> class. */
+        Stack<int> stack = new Stack<int>(); //Creates a stack
         while (true)
         {
             try
             {
                 Console.WriteLine("Insert a number to add (insert empty line or a letter to stop)");
                 int num = int.Parse(Console.ReadLine());
-                stack.Push(num);
-                counter++;
+                stack.Push(num); //Puts the number at the top
             }
             catch (FormatException)
             {
-                break;
+                break; //Breaks the loop when the input isn't a number
             }
 
         }
         Console.WriteLine("Reverse order:");
-        while (stack.Count > 0)
+        while (stack.Count > 0) //Loop to print numbers
         {
             int display = stack.Pop();
-            Console.WriteLine(display);
+            Console.WriteLine(display); 
         }
 
 
