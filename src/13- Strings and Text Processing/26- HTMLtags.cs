ecf92905f0f8bcca2d08d7e2e8fc4fdaa871775c
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-11-19 00:35:01 -0300

    Program that removes html tags of a string.

--- src/13- Strings and Text Processing/26- HTMLtags.cs
+++ src/13- Strings and Text Processing/26- HTMLtags.cs
@@ -8,9 +8,9 @@ attribute values from an HTML document.*/
 	public static void Main()
 	{
 		string html = "<body><h1>Comando: </h1></body><script>console.log()</script>";
-		string pattern = "<.*?>"; //sequencia
-		string textoLimpo = Regex.Replace(html, pattern, string.Empty); //trocar texto
-		Console.WriteLine(textoLimpo);
+		string pattern = "<.*?>"; 
+		string clean = Regex.Replace(html, pattern, string.Empty); //Switches text
+		Console.WriteLine(clean); //Displays the clean tex
 	}
 }
 
