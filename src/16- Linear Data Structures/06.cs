af8bee13d5483744671a9f73175f554312ec0a43
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-11-29 20:01:07 -0300

    Create 06.cs

---
+++ src/16- Linear Data Structures/06.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] array = {4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2};
+        array = InsertValues(array);
+
+        foreach(int num in array) //Display
+        {
+            Console.Write(num + " ");
+        }
+
+    }
+    static int[] InsertValues(int[] array)
+    {
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+        List<int> list = new List<int>();
+        foreach (int num in array)
+        {
+            if (dictionary.ContainsKey(num)) 
+            {
+                dictionary[num]++;
+            }
+            else
+            {
+                dictionary[num] = 1; //Creates key and count
+            }
+        }
+        foreach (var num in array)
+        {
+            if (dictionary[num] % 2 == 0)
+            {
+                list.Add(num);
+            }
+        }
+        return list.ToArray();
+
+    }
+    
+
+}
