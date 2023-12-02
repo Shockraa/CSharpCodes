06e764a54de54f18f73ef74a057a0863c0804988
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-12-02 16:38:05 -0300

    Update 10.cs

--- src/16- Linear Data Structures/10.cs
+++ src/16- Linear Data Structures/10.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Collections.Generic;
 
 public class Program
@@ -20,18 +19,30 @@ public class Program
         int current = 0;
 
         Queue<int> queue = new Queue<int>();
+        HashSet<int> check = new HashSet<int>();
         queue.Enqueue(N);
+        check.Add(N);
 
         while (current != M)
         {
             current = queue.Dequeue();
             Console.Write(current + " ");
 
-            queue.Enqueue(current * 2);
-            queue.Enqueue(current + 2);
-            queue.Enqueue(current + 1);
-
+            if (current * 2 <= M && !check.Contains(current * 2))
+            {
+                queue.Enqueue(current * 2);
+                check.Add(current * 2);
+            }
+            if (current + 2 <= M && !check.Contains(current + 2))
+            {
+                queue.Enqueue(current + 2);
+                check.Add(current + 2);
+            }
+            if (current + 1 <= M && !check.Contains(current + 1))
+            {
+                queue.Enqueue(current + 1);
+                check.Add(current + 1);
+            }
         }
     }
-
 }
