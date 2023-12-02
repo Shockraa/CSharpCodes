e52e748ccbf02e9b4cf8c127ff82bf0b33c9c70a
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-12-02 14:45:46 -0300

    Program that writes occurrences of numbers in an array.

--- src/16- Linear Data Structures/07- Occurrences.cs
+++ src/16- Linear Data Structures/07- Occurrences.cs
@@ -6,10 +6,11 @@ public class Program
 {
     public static void Main()
     {
+        //Write a program that finds in a given array of integers (in the range [0…1000]) how many times each of them occurs.
         int[] array = {4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2};
         AddValues(array);
 
-        foreach(int num in array) //Display Original Array
+        foreach(int num in array) //Display Original Array (ignore)
         {
             Console.Write(num + " ");
         }
@@ -17,7 +18,7 @@ public class Program
     }
     static void AddValues(int[] array)
     {
-        int[] occurrences = new int[1001];
+        int[] occurrences = new int[1001]; //Value from exercise
 
         foreach (var num in array)
         {
